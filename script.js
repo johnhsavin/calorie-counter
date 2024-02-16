283f138f3df39aabd5476dca2d38335a1d5518b6
John Savin <johnsavin@proton.me>
2024-02-16 09:51:00 -0500

    Refactor cleanInputString and add isInvalidInput functions

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
+
 function cleanInputString(str) {
-    const strArray = str.split('');
-    const cleanStrArray = [];
-
-    for (let i = 0; i < strArray.length; i++) {
-
-    }
-}
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+  }
+  
+  function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+  }
