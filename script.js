797ec64cff789e3112811afded3af0fb981d9497
John Savin <johnsavin@proton.me>
2024-05-05 00:31:04 -0400

    Update output display in script.js

--- script.js
+++ script.js
@@ -54,6 +54,12 @@ if(isError) return;
 const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
 const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
 const surplusOrDeficit = remainingCalories < 0 ? ("Surplus") : ("Deficit");
+output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+<hr></hr>
+<p>${budgetCalories} Calories Budgeted</p>
+<p>${consumedCalories} Calories Consumed</p>
+<p>${exerciseCalories} Calories Burned</p>
+`;
 
 
 function getCaloriesFromInputs(list) {
